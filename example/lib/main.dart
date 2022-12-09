2ddcb82b9931b3a3f73b34697b843d9241cb9b5f
Enes <meilhan@mifinity.com>
2022-12-09 13:12:11 +0000

    first

---
+++ example/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:my_package/my_package.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: MyPackage(),
+      ),
+    ),
+  ));
+}
---
+++ lib/my_package.dart
@@ -0,0 +1,10 @@
+library my_package;
+
+import 'package:flutter/widgets.dart';
+
+class MyPackage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('My Text Package');
+  }
+}
